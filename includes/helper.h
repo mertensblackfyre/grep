56f3d3e5197a514fd26d2e52792a7526ac3828bf
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-11-16 19:52:27 +0300

    Working on the translator

--- includes/helper.h
+++ includes/helper.h
@@ -7,4 +7,6 @@ extern int helper_string_int(std::string string);
 extern std::string helper_decimal_string(int decimal);
 extern void helper_append_file(std::string binary);
 extern int helper_delimiter_pos(const std::string &line, char delimiter);
-extern std::string helper_find_value(const std::string &key, const std::unordered_map<std::string, std::string> &table);
+extern std::string
+helper_find_value(const std::string &key,
+                  const std::unordered_map<std::string, std::string> &table);
